dfac1c041f04a875fd2fd49e60fedcbde1469373
Denis Lima <denis.a.lima@outlook.com>
2026-04-17 04:27:20 -0300

    feat: implementação do módulo de estoque e   dashboard inicial

--- apps/estoque/admin.py
+++ apps/estoque/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from apps.estoque.models.produto import Categoria, Produto
+from apps.estoque.models.movimento import MovimentoEstoque
+
+admin.site.register(Categoria)
+admin.site.register(Produto)
+admin.site.register(MovimentoEstoque)
