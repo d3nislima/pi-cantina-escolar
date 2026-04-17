9663aac3195d94157daee25b58391dc7d613691a
Denis Lima <denis.a.lima@outlook.com>
2026-04-17 05:06:01 -0300

    feat: implementação do módulo de vendas e ajustes   no estoque

--- apps/vendas/web/urls.py
+++ apps/vendas/web/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import VendaListView
+from .views import (
+    AdicionarItemView,
+    FinalizarVendaView,
+    NovaVendaView,
+    RemoverItemView,
+    VendaListView,
+)
 
 urlpatterns = [
     path("", VendaListView.as_view(), name="venda-list"),
+    path("nova/", NovaVendaView.as_view(), name="venda-create"),
+    path("adicionar-item/", AdicionarItemView.as_view(), name="venda-adicionar-item"),
+    path("remover-item/", RemoverItemView.as_view(), name="venda-remover-item"),
+    path("finalizar/", FinalizarVendaView.as_view(), name="venda-finalizar"),
 ]
