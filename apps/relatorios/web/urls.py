871b458c9c796fc8f82fafd46b6daa8b343d3bf5
Denis Lima <denis.a.lima@outlook.com>
2026-05-13 14:32:42 -0300

    feat: historico de vendas em relatorios

--- apps/relatorios/web/urls.py
+++ apps/relatorios/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MovimentacaoLogView, RelatoriosListView
+from .views import MovimentacaoLogView, RelatoriosListView, VendaListView
 
 urlpatterns = [
     path("", RelatoriosListView.as_view(), name="relatorios-list"),
     path("movimentacoes/", MovimentacaoLogView.as_view(), name="movimentacao-log"),
+    path("vendas/", VendaListView.as_view(), name="venda-list"),
 ]
--- apps/relatorios/web/views.py
+++ apps/relatorios/web/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, TemplateView
 
 from apps.estoque.models.movimento import MovimentoEstoque
+from apps.vendas.models.venda import Venda
 
 
 class RelatoriosListView(TemplateView):
@@ -12,4 +13,12 @@ class MovimentacaoLogView(ListView):
     template_name = "relatorios/movimentacao_log.html"
     context_object_name = "movimentacoes"
     ordering = ["-data_movimento"]
+    paginate_by = 50
+
+
+class VendaListView(ListView):
+    model = Venda
+    template_name = "vendas/venda_list.html"
+    context_object_name = "vendas"
+    ordering = ["-vendido_em"]
     paginate_by = 50
