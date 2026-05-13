edb26ad170f81407866d4cd0dcd790fe47a3266a
Denis Lima <denis.a.lima@outlook.com>
2026-05-13 02:33:02 -0300

    fix: corrigir refs de movimentacao-list e remover import não usado

--- apps/estoque/web/urls.py
+++ apps/estoque/web/urls.py
@@ -7,7 +7,6 @@ from .views import (
     CategoriaUpdateView,
     EstoqueListView,
     MovimentacaoCreateView,
-    MovimentacaoListView,
     ProdutoCreateView,
     ProdutoToggleView,
     ProdutoUpdateView,
